bf1f0e0e664598c55ff34900cd67aba8497566c5
Kalle Hallbäck <kalle.hallback@gmail.com>
2025-10-18 17:16:54 +0200

    corrected guessingspan

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
 	public static void main (String[] args) {
-		Guesser guesser = new Guesser(0, 100);
+		Guesser guesser = new Guesser(0, 1000);
 
 		guesser.start();
 	}
