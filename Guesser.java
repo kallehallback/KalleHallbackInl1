2256458336a7f59b6186874b51441aadd34f52ae
Kalle Hallbäck <kalle.hallback@gmail.com>
2025-10-18 15:14:07 +0200

    created main method with an instance of Guesser

--- Guesser.java
+++ Guesser.java
@@ -2,8 +2,8 @@ public class Guesser {
 	private int low;
 	private int high;
 
-	public int Guesser(int low, int high) {
+	public Guesser(int low, int high) {
 		this.low = low;
 		this.high = high;
 	}
-
+}
